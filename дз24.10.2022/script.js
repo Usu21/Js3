3aa6155e42cab4191267b23a410561e83acd0a68
Usu21 <98323022+Usu21@users.noreply.github.com>
2022-10-23 23:13:58 +0500

    Add files via upload

---
+++ дз24.10.2022/script.js
@@ -0,0 +1,25 @@
+//1
+function task(str) {
+    if (str >= 90 && str <= 100) {
+        console.log('A')
+    } else if (str >= 80) {
+        console.log('B')
+    } else if (str >= 70) {
+        console.log('C')
+    } else if (str >= 60) {
+        console.log('D')
+    } else if (str <= 59) {
+        console.log('F')
+    }
+}
+task('71')
+
+//2
+function lower(str2) {
+    console.log(str2.toLowerCase())
+}
+lower('ААА Привет Мир УУУ')
+
+
+//3
+// Не 3 я не понимаю как это сделать много что прогуглил посмотрел попробывал у меня не получилось честно :(
